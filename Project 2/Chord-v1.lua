8405ca88cf90b4646b18ee92ab746f77dbc801f4
splay <splay@splay-vbox.(none)>
2015-11-06 09:30:43 +0100

    Cosmetic changes (comments and explanations)

--- Project 2/Chord-v1.lua
+++ Project 2/Chord-v1.lua
@@ -1,12 +1,11 @@
 --
--- Created by IntelliJ IDEA.
--- User: splay
--- Date: 10/29/15
--- Time: 4:58 PM
--- To change this template use File | Settings | File Templates.
+-- Author:      Laurent Hayez
+-- Date:        29 oct. 2015
+-- Last Modif:  06 nov. 2015
+-- Description: Basic implementation of the chord ring.
 --
 
----[[
+--- [[
 require("splay.base")
 rpc = require("splay.urpc")
 misc = require("splay.misc")
@@ -17,7 +16,7 @@ if not job then
     -- can NOT be required in SPLAY deployments !
     local utils = require("splay.utils")
     if #arg < 2 then
-        print("lua "..arg[0].." my_position nb_nodes")
+        print("lua " .. arg[0] .. " my_position nb_nodes")
         os.exit()
     else
         local pos, total = tonumber(arg[1]), tonumber(arg[2])
@@ -25,7 +24,8 @@ if not job then
     end
     nodes = job.nodes()
 else
-   nodes = job.nodes()
+    nodes = job.nodes
+    on_cluster = true
 end
 
 rpc.server(job.me.port)
@@ -36,20 +36,20 @@ m = 32
 ------------------------------------------------
 -- computes hash value for a given input string
 function compute_hash(o)
-    return tonumber(string.sub(crypto.evp.new("sha1"):digest(o), 1, m / 4),16)
+    return tonumber(string.sub(crypto.evp.new("sha1"):digest(o), 1, m / 4), 16)
 end
 
 ------------------------------------------------
 
 ------------------------------------------------
----   ===            Variables           === ---
+--- ===            Variables           === ---
 ------------------------------------------------
 -- node itself
 n = job.me
 -- random position on the ring
 -- it is the hash of the concatenation of the node's ip and port
-n.id = compute_hash(n.ip..n.port)
-print("Node "..job.position.." id: "..n.id)
+n.id = compute_hash(n.ip .. n.port)
+print("Node " .. job.position .. " id: " .. n.id)
 -- closest node with bigger id
 successor = nil
 -- closest node with lower id
@@ -61,45 +61,51 @@ max_time = 180
 
 
 ------------------------------------------------
----   ===      Getters and Setters       === ---
+--- ===      Getters and Setters       === ---
 ------------------------------------------------
 
 ------------------------------------------------
 function get_successor()
     return successor
 end
+
 ------------------------------------------------
 ------------------------------------------------
 function get_predecessor()
     return predecessor
 end
+
 ------------------------------------------------
 ------------------------------------------------
 function set_successor(node)
     successor = node
 end
+
 ------------------------------------------------
 ------------------------------------------------
 function set_predecessor(node)
     predecessor = node
 end
+
 ------------------------------------------------
 
 -----------------------------------------------------------------------------
+-- Utility function that tests if the ring is correctly constructed
 var = false
 function test()
     if var == false then
-       var = true
-       print("Node ", job.position.."\'s successor id: ", ' ', successor.id)
-       events.sleep(2)
-       rpc.call(successor, {"test"})
+        var = true
+        print("Node ", job.position .. "\'s successor id: ", ' ', successor.id)
+        events.sleep(2)
+        rpc.call(successor, { "test" })
     end
 end
+
 -----------------------------------------------------------------------------
 
 
 ------------------------------------------------
----   ===        Chord functions         === ---
+--- ===        Chord functions         === ---
 ------------------------------------------------
 
 ------------------------------------------------
@@ -107,77 +113,70 @@ end
 -- Intervals can be (), (], [), [].
 -- A call to this function is for example: is_between(5, 4, 6, '(]')
 -- Returns true or false
+-- -----------------------
+-- Note: The functionnment of is_between is the same for all kind of brackets. Example for '()':
+-- We are on a ring, so if the lower bound of the interval is smaller than the upper bound, we
+-- simply check if the id is in (lower, upper).
+-- if upper < lower, the id may still be in the intervall, that simply means that 2^m is in the interval,
+-- and as we work modulo 2^m, upper is smaller, but modulo 2^m only. Think of a clock, where the hand of
+-- the clock might be between (11, 1). This interval is not empty. Thus in the case where upper <= lower,
+-- we have to check if id >= lower, or id <= upper. In the previous example, that means we check if the
+-- hand of the clock is between [11,12] or between [0, 1].
+-- -----------------------
 function is_between(nb, lower, upper, brackets)
-   if brackets == '()' then
-      if lower < upper then
-	 return (nb > lower) and (nb < upper)
-      else
-	 return (nb >= lower) or (nb <= upper)
-      end
-   elseif brackets == '(]' then
-      if lower < upper then
-	 return (nb > lower) and (nb <= upper)
-      else
-	 return (nb > lower) or (nb <= upper)
-      end
-   elseif brackets == '[)' then
-      if lower < upper then
-	 return (nb >= lower) and (nb < upper)
-      else
-	 return (nb >= lower) or (nb < upper)
-      end
-   else
-      if lower < upper then
-	 return (nb >= lower) and (nb <= upper)
-      else
-	 return (nb >= lower) or (nb <= upper)
-      end
-   end
+    if brackets == '()' then
+        if lower < upper then
+            return (nb > lower) and (nb < upper)
+        else
+            return (nb >= lower) or (nb <= upper)
+        end
+    elseif brackets == '(]' then
+        if lower < upper then
+            return (nb > lower) and (nb <= upper)
+        else
+            return (nb > lower) or (nb <= upper)
+        end
+    elseif brackets == '[)' then
+        if lower < upper then
+            return (nb >= lower) and (nb < upper)
+        else
+            return (nb >= lower) or (nb < upper)
+        end
+    else
+        if lower < upper then
+            return (nb >= lower) and (nb <= upper)
+        else
+            return (nb >= lower) or (nb <= upper)
+        end
+    end
 end
+
 ------------------------------------------------
-
-
-
-
-function isInRange(e, from, to)
-   if from < to then
-      return (e > from) and (e <= to) -- => (from, to]
-   else
-      return (e > from) or (e <= to)  -- => (from, to] over circle nPrime, also handles starting condition from == to
-   end
-end
-
-
 
 ------------------------------------------------
 -- ask node n to find id's predecessor
 function find_predecessor(id)
-
     local n1 = n -- start searching with self
     local n1_successor = successor
-    -- important: n1_successor may have smaller ID than n1
-    -- we check if the interval is not empty (otherwise we discard the solution) and then we apply the condition
-    -- that id is not in the interval (n1.id, n1_successor.id].
-    print("id: ", id, "(", n1.id, n1_successor.id,"]")
-    --while (not interval_is_empty(n1.id, n1_successor.id, '(]')) and (not is_between(id, n1.id, n1_successor.id, '(]')) do
-    while not is_between(id, n1.id, n1_successor.id, '(]')  do
+
+    while not is_between(id, n1.id, n1_successor.id, '(]') do
         n1 = n1_successor
-        n1_successor = rpc.call(n1, {"get_successor"}) -- invoke get_successor() on n1
-	print("id: ", id, "(", n1.id, n1_successor.id,"]")
+        n1_successor = rpc.call(n1, { "get_successor" }) -- invoke get_successor() on n1
     end
-    --print("Node "..job.position.."\'s successor id: "..successor.id)
-    --rpc.call(successor, {"test", n.id})
+
     return n1
 end
+
 ------------------------------------------------
- 
+
 ------------------------------------------------
 -- ask node n1 to find id's successor
 function find_successor(id)
     local n1 = find_predecessor(id)
-    local n1_successor = rpc.call(n1, {"get_successor"})
+    local n1_successor = rpc.call(n1, { "get_successor" })
     return n1_successor
 end
+
 ------------------------------------------------
 
 ------------------------------------------------
@@ -187,16 +186,17 @@ end
 function init_neighbours(n1)
     -- find (through node n1) the successor of your ID
     -- call find_successor on n1 with parameter (n.id+1)%2^m
-    successor = rpc.call(n1, {"find_successor", (n.id+1)%(2^m)})
+    successor = rpc.call(n1, { "find_successor", (n.id + 1) % (2 ^ m) })
     -- call get_predecessor on node successor
-    predecessor = rpc.call(successor, {"get_predecessor"})
+    predecessor = rpc.call(successor, { "get_predecessor" })
 
     -- update the neighbours
     -- call set_predecessor(n) on successor
-    rpc.call(successor, {"set_predecessor", n})
+    rpc.call(successor, { "set_predecessor", n })
     -- call set_successor(n) on predecessor
-    rpc.call(predecessor, {"set_successor", n})
+    rpc.call(predecessor, { "set_successor", n })
 end
+
 ------------------------------------------------
 
 ------------------------------------------------
@@ -205,15 +205,13 @@ end
 function join(n1)
     if n1 then
         init_neighbours(n1)
-	print("Node "..job.position.." id :"..n.id)
-	print("Node "..job.position.." successor id :"..successor.id)
-	print("Node "..job.position.." predecessor id :"..predecessor.id)
-    -- n is the only node in the network
+        -- n is the only node in the network
     else
         successor = n
         predecessor = n
     end
 end
+
 ------------------------------------------------
 
 
@@ -223,44 +221,50 @@ function terminator()
     events.sleep(max_time)
     os.exit()
 end
+
 -----------------------------------------------------------------------------
 
 
 -----------------------------------------------------------------------------
 function main()
 
+    if on_cluster then
+        -- sleep 10 minutes so that all the nodes on the cluster are ready.
+        events.sleep(600)
+    end
+
     -- This is the first node that every other node knows
     n0 = nodes[1]
     -- If n is the first node, it immediately joins the ring
     if job.position == 1 then
-       -- The first node creates the Ring
-       print("Node 1 is creating the ring...\n")
-       join(nil)
-    -- otherwise, wait a random time between [0,10) seconds to join the ring
+        -- The first node creates the Ring
+        print("Node 1 is creating the ring...\n")
+        join(nil)
+        -- otherwise, wait a random time between [0,10) seconds to join the ring
     else
-       -- Wait 2 seconds for the ring to be initialized
-       events.sleep(2)
+        -- Wait 2 seconds for the ring to be initialized
+        events.sleep(2)
         -- Generate a random seed
-        math.randomseed(job.position*os.time())
+        math.randomseed(job.position * os.time())
         -- wait a random time in [0,8) seconds to join the ring (=> total waited <= 10)
-        wait_time = math.random()*8
+        wait_time = math.random() * 8
         events.sleep(wait_time)
-	--events.sleep(job.position)
-	print("Node " .. job.position .. " joins the ring after waiting " .. (wait_time+2) .. " seconds.\n")
+        print("Node " .. job.position .. " joins the ring after waiting " .. (wait_time + 2) .. " seconds.\n")
         join(n0)
     end
 
     if job.position == 1 then
-       events.sleep(15)
-       rpc.call(successor, {"test"})
+        if on_cluster then
+            -- wait 3 minutes for latency.
+            events.sleep(180)
+        end
+        events.sleep(15)
+        rpc.call(successor, { "test" })
     end
-
-   -- print("\n\nNode "..job.position.."\'s id: "..n.id)
-   -- print("Node "..job.position.."\'s successor id: "..successor.id)
-   -- print("Node "..job.position.."\'s predecessor id: "..predecessor.id.."\n")
 
     events.thread(terminator)
 end
+
 -----------------------------------------------------------------------------
 
 -- execute the main function
